25d21c90ececbaa3d4224d0712130f0e615e40e0
j_shum55@outlook.com <j_shum55@outlook.com>
2017-07-14 11:52:14 -0400

    Fixed tail call rec for problem 2

--- problems 1-50/even_sum_fib.py
+++ problems 1-50/even_sum_fib.py
@@ -1,9 +1,12 @@
 def fib_even_sum(n):
-    return fib_tail(0, 1, n, 0)
+    '''
+    n is the max fibonacci value
+    '''
+    return fib_even_sum_tail(0, 1, n, 0)
 
 def fib_even_sum_tail(a, b, n, even_sum):
     even_sum += a if a % 2 == 0 else 0
-    return fib_tail(b, a + b, n - 1, even_sum) if n > 0 else a
+    return fib_even_sum_tail(b, a + b, n, even_sum) if b < n else even_sum
 
 def fib_even_sum_iter(n, a, b):
     even_sum = 0
@@ -11,8 +14,3 @@ def fib_even_sum_iter(n, a, b):
         a, b = b, a+b
         even_sum += a if a % 2 == 0 else 0
     return even_sum
-
-     
-        
-        
-        
